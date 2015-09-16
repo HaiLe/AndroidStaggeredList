d7f19842345d507ea5838e3fa3cfcff980e4e981
Hai Le <honghaile@gmail.com>
2015-09-16 16:16:45 -0700

    display a staggered list

--- app/src/main/java/goreach/me/staggeredlist/MainActivity.java
+++ app/src/main/java/goreach/me/staggeredlist/MainActivity.java
@@ -1,37 +1,48 @@
 package goreach.me.staggeredlist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private StaggeredGridLayoutManager mLayoutManager;
+
+    private ArrayList<String> myDataset = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_staggered_list);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_staggered_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        StaggeredGridLayoutManager mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL); // (int spanCount, int orientation)
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new StaggeredListAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        addDataToDataSet();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void addDataToDataSet() {
+        myDataset.clear();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (int i = 0; i < 10; i++) {
+            myDataset.add("Item " + Integer.toString(i));
         }
 
-        return super.onOptionsItemSelected(item);
+        mAdapter.notifyDataSetChanged();
     }
 }
